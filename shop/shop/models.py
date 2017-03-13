53858e69756b15bd26eab3214c035db5115407d6
Alexey Ruzin <ruzin@kokoc.com>
2017-03-13 07:17:05 +0300

    models fix2

--- shop/shop/models.py
+++ shop/shop/models.py
@@ -1,9 +1,11 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.title
@@ -15,6 +17,10 @@ class Item(models.Model):
     description = models.CharField(max_length=200)
     price = models.DecimalField(default=0, max_digits=12, decimal_places=2)
     in_stock = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date added', auto_now_add=True)
 
     def __str__(self):
         return "{} ({})".format(self.title, self.price)
+
+    def is_new(self):
+        return self.pub_date > timezone.now() - timedelta(days=1)
