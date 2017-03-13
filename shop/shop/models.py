e28dd0c4c625d2860d627ff5ea5fe0d3998b753a
Alexey Ruzin <ruzin@kokoc.com>
2017-03-13 06:53:15 +0300

    models

--- shop/shop/models.py
+++ shop/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Item(models.Model):
+    title = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    in_stock = models.IntegerField(default=0)
