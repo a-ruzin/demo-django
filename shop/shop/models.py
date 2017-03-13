e7a1deccab693513efceb1f5682842d51f1e06ef
Alexey Ruzin <ruzin@kokoc.com>
2017-03-13 06:59:04 +0300

    models fix

--- shop/shop/models.py
+++ shop/shop/models.py
@@ -5,9 +5,16 @@ class Category(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.title
+
 
 class Item(models.Model):
-    title = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     price = models.DecimalField(default=0, max_digits=12, decimal_places=2)
     in_stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{} ({})".format(self.title, self.price)
